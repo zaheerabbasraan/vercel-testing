e24610466c9d025de2af5f0fab8bb3dd8a19bebe
Zaheer Abbas <amirabbasraan@gmail.com>
2022-11-01 21:34:23 +0500

    initial

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.use(function(req, res, next) {
     res.render('error');
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
 })
